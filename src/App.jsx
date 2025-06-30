4db42ae61e599e71aabb1de5bff86ad903047b26
JonaVicesar <jonathan.vicesar@fiuni.edu.py>
2025-06-30 20:35:38 -0300

    structure for the page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SpotifyDNAApp from './components/SpotifyDNAApp.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <SpotifyDNAApp />
+    </div>
+  )
+}
+export default App
