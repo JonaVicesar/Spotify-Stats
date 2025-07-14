0e9cd35fc7acaacade133d59d95bc6b1ccfd9c84
JonaVicesar <jonathan.vicesar@fiuni.edu.py>
2025-07-14 09:37:41 -0300

    feat: implement playPlaylist function

--- src/services/spotifyActions.js
+++ src/services/spotifyActions.js
@@ -1,8 +1,11 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { Contact } from "lucide-react";
+
 /**
  * funcion para hacer las peticiones a la api(helper)
  */
 const spotifyApiRequest = async (endpoint, options = {}) => {
-  const token = localStorage.getItem('spotify_access_token');
+  const token = localStorage.getItem("spotify_access_token");
 
   if (!token) {
     throw new Error("No hay token de acceso");
@@ -25,7 +28,15 @@ const spotifyApiRequest = async (endpoint, options = {}) => {
   return response.json();
 };
 
-/*const userId = await spotifyApiRequest('/me');*/
+/**
+ * obtener el userId
+ */
+let userId = null;
+const getUserId = async () => {
+  const response = await spotifyApiRequest("/me");
+  userId = response;
+  return userId;
+};
 
 /**
  * REPRODCIR CANCION
@@ -74,43 +85,80 @@ export const playTrack = async (trackId, contextUri = null) => {
  * AGREGAR A UNA PLAYLIST
  */
 export const addToPlayList = async (trackId) => {
-    //TODO
+  //TODO
 };
 
 /**
  * VER ALBUM
  */
-export const viewAlbum = (albumId) =>{
-    //abrir el album
-    window.open(`https://open.spotify.com/album/${albumId}`, '_blank')
-}
+export const viewAlbum = (albumId) => {
+  //abrir el album
+  window.open(`https://open.spotify.com/album/${albumId}`, "_blank");
+};
 
 /**
  * VER ARTISTA
  */
 export const viewArtist = (artistId) => {
-    //abrir el perfil del artista
-    window.open(`https://open.spotify.com/artist/${artistId}`, 'blank');
-}
+  //abrir el perfil del artista
+  window.open(`https://open.spotify.com/artist/${artistId}`, "_blank");
+};
 
 /**
  * SEGUIR AL ARTISTA
  */
-export const followArtist = async(artistId) =>{
-    try {
-        await spotifyApiRequest('/me/following', {
-            method: 'PUT',
-            body: JSON.stringify({
-                type: 'artist',
-                ids: [artistId]
-            })
-        });
+export const followArtist = async (artistId) => {
+  try {
+    await spotifyApiRequest("/me/following", {
+      method: "PUT",
+      body: JSON.stringify({
+        type: "artist",
+        ids: [artistId],
+      }),
+    });
 
-        alert(`Ya sigues a ${artistId}`);
+    alert(`Ya sigues a ${artistId}`);
+  } catch (error) {
+    console.error("Error al intentar seguir al artista:", error);
+    alert("Error al trata de seguir al artista");
+  }
+};
 
-    }catch(error){
-        console.error('Error al intentar seguir al artista:', error);
-        alert('Error al trata de seguir al artista')
+/**
+ * GUARDAR ALBUM
+ */
+export const saveAlbum = ""; //TODO
+
+
+/**
+ * REPRODUCIR PLAYLIST
+ */
+export const playPlaylist = async (playlistId) => {
+
+  try {
+    const devices = await spotifyApiRequest("me/player/devices");
+
+    if (!devices || devices.devices.length === 0){
+
+      //si no hay dispositivos abrimos spotify wed
+      window.open(`https://open.spotify.com/playlist/${playlistId}`, '_blank')
+      return;
     }
-}
 
+    //ver un dispositivo activo
+    const activeDevice = devices.devices.find((d) => d.is_active) || devices.devices[0];
+
+    await spotifyApiRequest('me/player/play', {
+      method: 'PUT',
+      body: JSON.stringify({
+        device_id:activeDevice.id,
+        contextUri: `spotify:playlist:${playPlaylist}`
+      })
+    });
+
+  }catch(error){
+    console.error("Ocurrio un error intentando reproducir la playlist", error);
+    window.open(`https://open.spotify.com/playlist/${playlistId}`, '_blank');
+  }
+
+}
